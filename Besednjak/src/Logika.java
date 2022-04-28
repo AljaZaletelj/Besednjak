615c75668f815964c4c7acb421dbc39e1ab06c02
Simon Grad <simon.grad3@gmail.com>
2022-04-28 16:29:43 +0200

    shranjeno

--- Besednjak/src/Logika.java
+++ Besednjak/src/Logika.java
@@ -2,15 +2,12 @@
 public class Logika {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	
-	protected String z = "zelena";
-	protected String r = "rumena";
-	protected String b = "bela";
-	protected int steviloPoskusov = 5;
+	
 	
 	
 	// funkcija izberiKljuc - iz datoteke besed izbere neko besedo.
