6d102195215e315beb16145cadf4c5d7729b6e36
Simon Grad <simon.grad3@gmail.com>
2022-05-11 13:29:52 +0200

    Prvič ko dela

--- Besednjak/src/Logika.java
+++ Besednjak/src/Logika.java
@@ -1,13 +1,38 @@
-public class Logika {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.ThreadLocalRandom;
 
-	static String geslo = "DREVO";
+public class Logika {
 	
 	
-	public static String dolociGeslo() {
+	public static String izberiGeslo(String imeVhod) throws IOException {
+		BufferedReader vhod = new BufferedReader(new FileReader(imeVhod));
+		String geslo = "";
 		
+		while (vhod.ready()) {
+			String vrstica = vhod.readLine().trim();
+			String[] besede = vrstica.split(",");
+			int max = besede.length;
+			int randomNum = ThreadLocalRandom.current().nextInt(0, max);
+			geslo = besede[randomNum];
+		}
+		
+		
+		vhod.close();
+		return geslo.toUpperCase();
 	}
 	
+	
 	public static Besednjak novaIgra() {
+		String geslo = "";
+		try {
+			geslo = izberiGeslo("src/besedle.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		Besednjak igra = new Besednjak(geslo);
 		return igra;
 	}
