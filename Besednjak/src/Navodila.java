7bd372498bf8846ecbc840d894367ba73447bf4d
Simon Grad <simon.grad3@gmail.com>
2022-06-15 11:29:51 +0200

    Slovnični popravki navodil

--- Besednjak/src/Navodila.java
+++ Besednjak/src/Navodila.java
@@ -1,16 +1,20 @@
 
 public class Navodila {
 	
-	public static String navodilo = "Pozdravljeni v igri besednjak.\n"
-			+ "Tvoja naloga je, da poskušaš ugotoviti skrito geslo, ki je sestavljeno iz petih črk. "
-			+ "Tvoji poskusi morajo biti prav tako sestavljeni iz petih črk slovenske abecede.\n"
-			+ "\nTvoj poskus se bo zapisal v mrežo, v kateri bo vsaka črka tvojega poskusa v svojem kvadratku. Ta pa je lahko različne barve. Če je: "
-			+ "\n - SIV, to pomeni, da te črke v iskanem geslu ni. "
-			+ "\n - RUMEN, to pomeni, da je ta črka v iskanem geslu, vendar je na tem mestu ni. "
-			+ "\n - ZELEN, da si pravilno uganil tudi mesto te črke v geslu."
+	public static String navodilo = "Pozdravljeni v igri Besednjak!\n"
+			+ "Cilj igre je ugotoviti skrito geslo, ki je sestavljeno iz petih črk. "
+			+ "Tvoji poskusi morajo biti sestavljeni iz petih črk slovenske abecede.\n"
+			+ "\nTvoj poskus se bo zapisal v mrežo, v kateri bo vsaka črka poskusa zapisala v svoj kvadratek in bo obarvana."
+			+ " Če je kvadratek: "
+			+ "\n - SIV, te črke v iskanem geslu ni. "
+			+ "\n - RUMEN, potem je ta črka v iskanem geslu, vendar ne na tem mestu. "
+			+ "\n - ZELEN, si pravilno uganil črko in tudi njeno mesto v geslu."
 			+ "\n\nPOZOR:"
-			+ "\nOb primeru, da je v geslu samo en A na zadnjem mestu, tvoj poskus pa je bil TRAVA. "
-			+ "\nV tem primeru, "
-			+ " se bo prvi A v trAva pobarval na rumeno, drugi A v travA pa zeleno.";
+			+ "\nBarvanje črk ni povezano s številom ponovitev črk v geslu."
+			+ "\n   Primer:"
+			+ "\n    Tvoj poskus je TRAVA, geslo pa OPERA. "
+			+ "\n    V tem primeru, "
+			+ " se bo v mreži tretji kvadratek obarval rumeno, zadnji pa zeleno."
+			+ "\n    To ne pomeni, da se črka A v geslu pojavi dvakrat!";
 
 }
